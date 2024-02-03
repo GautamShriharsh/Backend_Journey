30d7dab94368bd0724030c4ec2ab2515e061114d
Shriharsh <shriharshgautam7@gmail.com>
2024-02-03 13:13:15 +0530

    Created app using express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Backend First");
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/twitter', (req,res) => {
+    res.send('xdotcom')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
