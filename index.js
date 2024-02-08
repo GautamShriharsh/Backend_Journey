db158fd74b9103335f774a37b90efd9ff317d41a
Shriharsh <shriharshgautam7@gmail.com>
2024-02-08 14:44:13 +0530

    Created and imported env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 
 const app = express()
@@ -5,13 +6,21 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello world!')
 })
 
 app.get('/twitter', (req,res) => {
     res.send('xdotcom')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.get('/login', (req, res) => {
+    res.send('<h1>Please login at the website</h1>')
+})
+
+app.get('/youtube', (req,res) => {
+  res.send('<h2>Coding and learning Express</h2>')
+})
+
+app.listen(process.env.PORT , () => {
+  console.log(`Example app listening on port ${process.env.PORT}`)
 })
